ad4bb7dabee9dde3b9c30d5e39223c302080d077
Sunny <sunny6464n@gmail.com>
2024-03-23 12:03:53 +0530

    add : projects section

---
+++ src/constants/constants.ts
@@ -0,0 +1,99 @@
+export const EDUCATION = [
+    {
+        startDate: 2023,
+        endDate: 2026,
+        education: 'BTech',
+        course: 'Computer Science & Engineering',
+        description: 'Pursuing Btech....',
+        graduatedFrom: 'St Solider Institute of Engineering and Technology',
+        grade: '7.5'
+    },
+    {
+        startDate: 2020,
+        endDate: 2023,
+        education: 'Diploma',
+        course: 'Computer Science & Engineering',
+        description: 'Pursuing Diploma....',
+        graduatedFrom: 'Mehr Chand Polytechnic College',
+        grade: '7.5'
+    },
+    {
+        startDate: 2019,
+        endDate: 2020,
+        education: 'Matriculation',
+        course: null,
+        description: 'Completed Matriculation',
+        graduatedFrom: 'A.P.S Public Senior Secondary School',
+        grade: 'A+'
+    },
+];
+
+export const EXPERIENCE = [
+    {
+        company: 'Qservices',
+        startDate: 'April 2023',
+        endDate: 'Nov 2023',
+        experienceType: 'Internship',
+        description: 'explantion',
+        profile: 'Nodejs + Blockchain Developer',
+    },
+    {
+        company: 'O7 Services',
+        startDate: 'Aug 2022',
+        endDate: 'Feb 2023',
+        experienceType: 'Training',
+        description: 'explantion',
+        profile: 'Full Stack Developer'
+    },
+    {
+        company: 'O7 Services',
+        startDate: 'July 2022',
+        endDate: 'Aug 2022',
+        experienceType: 'Industrial Training',
+        description: 'explantion',
+        profile: 'Core PHP'
+    },
+];
+
+export const PROJECTS = [
+    {
+        name : 'NFT Marketplace', 
+        imageUrl : 'nil', 
+        description : 'describe the project', 
+        url : 'url', 
+        github : 'github link', 
+        techStack : 'Reactjs, Nodejs, MongoDB, Solidity, Ethereum, Firebase'
+    }, 
+    {
+        name : 'News App', 
+        imageUrl : 'nil', 
+        description : 'describe the project', 
+        url : 'url', 
+        github : 'github link', 
+        techStack : 'Reactjs, Nodejs, Tailwind CSS'
+    }, 
+    {
+        name : 'Journal Writing', 
+        imageUrl : 'nil', 
+        description : 'describe the project', 
+        url : 'url', 
+        github : 'github link', 
+        techStack : 'Php, html, css, js'
+    }, 
+    // {
+    //     name : 'Snappy Stories', 
+    //     imageUrl : 'nil', 
+    //     description : 'describe the project', 
+    //     url : 'url', 
+    //     github : 'github link', 
+    //     techStack : 'Nodjes, Ejs, OAuth'
+    // }, 
+    // {
+    //     name : 'Secrets', 
+    //     imageUrl : 'nil', 
+    //     description : 'describe the project', 
+    //     url : 'url', 
+    //     github : 'github link', 
+    //     techStack : 'Reactjs, Nodejs, MongoDB'
+    // }, 
+]
