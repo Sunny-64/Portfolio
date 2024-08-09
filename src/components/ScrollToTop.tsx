d0d6266bfa18c83c99b0b24d3e609372afec88b1
Sunny-64 <sunny6464n@gmail.com>
2024-08-10 03:12:30 +0530

    minor fix

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -8,7 +8,7 @@ const ScrollToTop = () => {
 
     const scroll = () => window.scrollTo(0, 0);
     return (
-        <a onClick={scroll} className={`bg-cerulean transition-opacity duration-300  ${Math.round(scrollYPos) > 600 ? 'opacity-90 hover:cursor-pointer': 'opacity-0'} font-bold text-2xl fixed bottom-10 right-10 w-12 h-12 rounded-md flex justify-center items-center `}><IoArrowUpCircleSharp /></a>
+        <button onClick={scroll} className={`bg-cerulean transition-opacity duration-300  ${Math.round(scrollYPos) > 600 ? 'opacity-90 hover:cursor-pointer': 'opacity-0'} font-bold text-2xl fixed bottom-10 right-10 w-12 h-12 rounded-md flex justify-center items-center `}><IoArrowUpCircleSharp /></button>
     )
 }
 
