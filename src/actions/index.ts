c1dacbd8b7abd4ccfe581ac57bbe92591c5ad8f8
Sunny-64 <sunny6464n@gmail.com>
2024-08-26 23:22:18 +0530

    add nodemailer

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,6 +3,15 @@
 import { connectToMongoDB } from '@/lib/db';
 import { Contact, IContact } from '@/models/contact.model';
 import { revalidatePath } from 'next/cache';
+import nodemailer from 'nodemailer'; 
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS,
+    },
+});
 
 export const createMessage = async ({ email, message }: IContact) => {
     await connectToMongoDB();
@@ -12,6 +21,13 @@ export const createMessage = async ({ email, message }: IContact) => {
             message,
         });
         const isSaved = await newMessage.save();
+        await transporter.sendMail({
+            to: process.env.SEND_TO, 
+            from: process.env.GMAIL_USER, 
+            subject: 'Message Received from Portfolio', 
+            html: `<h4>${isSaved.email}</h4> </br>
+                    <p>${isSaved.message}</p>`
+        }); 
         revalidatePath('/');
         return { message: 'Message Sent' };
     } catch (err) {
